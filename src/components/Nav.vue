a96cbf904d48fe56a262430009e524decbf258f2
Rain <448130604@qq.com>
2022-01-12 17:52:31 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="nav">
-    <router-link to="/details">明细</router-link>
+    <router-link to="/details">
+      <Icon name="details"/>
+      明细
+    </router-link>
     |
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+
+
   export default {
     name: 'Nav'
   };
