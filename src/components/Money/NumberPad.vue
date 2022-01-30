a61f4add62a225078ca9ad29d611053051ce7fa6
Rain <448130604@qq.com>
2022-01-30 13:14:44 +0800

    重写了NumberPad，未作加减

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -10,210 +10,60 @@
       </div>
     </div>
     <div class="buttons">
-      <button
-        v-for="(name, index) in buttonList" :key="index"
-        :class="name==='完成' && 'ok'"
-        @click="handle(name)"
-      >
-        {{ name }}
-      </button>
-      <button @click="remove">
-        ←
-      </button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button class="ok" @click="ok">OK</button>
+      <button class="zero" @click="inputContent">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component} from 'vue-property-decorator';
 
   @Component
   export default class NumberPad extends Vue {
-    @Prop(String) classPrefix?: string;
-    @Prop({required: true, type: String}) note!: string;
-    @Prop({required: true, type: Number}) amount!: string;
-    buttonList: string[] = ['7', '8', '9', '+', '4', '5', '6', '-',
-      '1', '2', '3', '完成', '.', '0'];
     output = '0';
-    dot = true;
-    // 有效数字最多为.前6位.后2位
-    validNumberBeforeDot = 6;
-    validNumberAfterDot = 2;
-    // 记录运算
-    beforeNumber = 0;
-    beforeIndex = 0;
-    operator = '';
-    writeNote(note: string) {
-      this.$emit('update:note', note);
-    }
-    handle(button: string) {
-      if (button !== '完成') {
-        this.handleInput(button);
-      } else {
-        this.complete();
-      }
-    }
-    handleInput(button: string) {
-      // 最大有效位数21位
-      if (this.output.length >= 21) {
-        return;
-      }
-      // 数字不能以0开头(除了小数)
+    inputContent(event: MouseEvent) {
+      const button = (event.target as HTMLButtonElement);
+      const input = button.textContent!;
+      if (this.output.length === 10) { return; }
       if (this.output === '0') {
-        if ('0123456789'.indexOf(button) >= 0) {
-          this.output = button;
-          this.validNumberBeforeDot -= 1;
-        } else if (button === '.') {
-          this.output += button;
-          this.dot = false;
-        }
-        return;
-      }
-      // 处理输入为点
-      if (button === '.') {
-        return this.handleDot();
-      }
-      // 处理数字
-      if ('0123456789'.indexOf(button) >= 0) {
-        return this.handleNumber(button);
-      }
-      // 处理运算符
-      if ('+-'.indexOf(button) >= 0) {
-        return this.handleOperator(button);
-      }
-    }
-    handleDot() {
-      if (this.dot) {
-        this.output += '.';
-        this.dot = false;
-      }
-    }
-    handleNumber(num: string) {
-      if (this.dot && this.validNumberBeforeDot > 0) {
-        this.output += num;
-        this.validNumberBeforeDot -= 1;
-      } else if (!this.dot && this.validNumberAfterDot > 0) {
-        this.output += num;
-        this.validNumberAfterDot -= 1;
-      }
-    }
-    handleOperator(operator: string) {
-      const last = this.output.slice(-1);
-      // 不合法输入
-      if ('.+-'.indexOf(last) >= 0) {
-        return;
-      } else {
-        if (this.operator !== '') {
-          // 对上一个表达式进行运算
-          this.calculate();
-          this.operator = operator;
-          this.reset();
-          this.beforeNumber = this.handleError(this.beforeNumber);
-          this.output = this.beforeNumber.toString() + operator;
-          this.beforeIndex = this.output.length;
-          return;
+        if ('0123456789'.indexOf(input) >= 0) {
+          this.output = input;
         } else {
-          // 记录上一个数
-          this.beforeNumber = parseFloat(this.output);
-          this.operator = operator;
-          this.output += operator;
-          this.beforeIndex = this.output.length;
-          return;
+          this.output += input;
         }
+        return;
       }
-    }
-    // 处理舍入误差
-    handleError(value: number) {
-      const integer = value.toString().split('.')[0];
-      let decimal = value.toString().split('.')[1];
-      let wholeNumber: string;
-      if (decimal) {
-        // 有效位
-        const index = decimal.indexOf('999999');
-        if (index >= 0) {
-          decimal = decimal.slice(0, index);
-          const last = decimal.slice(-1);
-          decimal = decimal.slice(0, -1) + (parseInt(last) + 1).toString();
-        }
-        if (decimal.length > 2) {
-          decimal = decimal.slice(0, 2); // 只保留2位
-        }
-        wholeNumber = [integer, decimal].join('.');
-      } else {
-        wholeNumber = integer;
-      }
-      return parseFloat(wholeNumber);
-    }
-    // 计算
-    calculate() {
-      const afterNumber = parseFloat(this.output.slice(this.beforeIndex));
-      if (this.operator === '+') {
-        this.beforeNumber = this.beforeNumber + afterNumber;
-      } else if (this.operator === '-') {
-        this.beforeNumber = this.beforeNumber - afterNumber;
-      }
-    }
-    complete() {
-      const last = this.output.slice(-1);
-      if ((this.output.indexOf('+') >= 0 || this.output.indexOf('-') >= 0) && '+-'.indexOf(last) < 0) {
-        this.calculate();
-        this.beforeNumber = this.handleError(this.beforeNumber);
-      } else {
-        this.beforeNumber = parseFloat(this.output);
-      }
-      this.$emit('update:amount', this.beforeNumber);
-      this.output = '0';
-      this.$emit('complete');
+      if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+      this.output += input;
     }
     remove() {
-      if (this.output.length > 1) {
-        const last = this.output.slice(-1);
-        if (last === '.') {
-          this.dot = true;
-        } else if ('+-'.indexOf(last) >= 0) {
-          this.operator = '';
-        } else if ('0123456789'.indexOf(last) >= 0) {
-          if (this.dot) {
-            this.validNumberBeforeDot += 1;
-          } else {
-            this.validNumberAfterDot += 1;
-          }
-        }
-        this.output = this.output.slice(0, -1);
-      } else {
+      if (this.output.length === 1) {
         this.output = '0';
-        this.reset();
-      }
-    }
-    reset() {
-      this.dot = true;
-      this.validNumberBeforeDot = 6;
-      this.validNumberAfterDot = 2;
-    }
-    @Watch('operator')
-    onOperator(val: string) {
-      // 操作符被删除
-      if (val === '') {
-        // 重置 dot 以及 validNumber 状态
-        if (this.beforeNumber.toString().indexOf('.') >= 0) {
-          this.dot = false;
-          const integer = this.beforeNumber.toString().split('.')[0];
-          const decimal = this.beforeNumber.toString().split('.')[1];
-          this.validNumberBeforeDot = 6 - integer.length;
-          this.validNumberAfterDot = 2 - decimal.length;
-        } else {
-          this.dot = true;
-          this.validNumberBeforeDot = 6 - this.beforeNumber.toString().length;
-          this.validNumberAfterDot = 2;
-        }
       } else {
-        // 有新的操作符
-        // dot 及 validNumber置为初始状态
-        this.reset();
+        this.output = this.output.slice(0, -1);
       }
     }
-  }</script>
+    clear() {
+      this.output = '0';
+    }
+    ok() {
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
@@ -255,6 +105,9 @@
           height: 64*2px;
           float: right;
         }
+        &.zero {
+          width: 25*2%;
+        }
         $bg: lighten($color-highlight, 10%) ;
         &:nth-child(1) {
           background: $bg;
@@ -265,13 +118,13 @@
         &:nth-child(3), &:nth-child(6), &:nth-child(9) {
           background: darken($bg, 4*2%);
         }
-        &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(13) {
+        &:nth-child(4), &:nth-child(7), &:nth-child(10) {
           background: darken($bg, 4*3%);
         }
-        &:nth-child(8), &:nth-child(11), &:nth-child(14) {
+        &:nth-child(8), &:nth-child(11), &:nth-child(13) {
           background: darken($bg, 4*4%);
         }
-        &:nth-child(15) {
+        &:nth-child(14) {
           background: darken($bg, 4*5%);
         }
         &:nth-child(12) {
