ab2db31822a5480666f33606c239bac899c93af3
Rain <448130604@qq.com>
2022-02-01 22:01:32 +0800

    把record的复制保存到localstorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,7 @@
     {{ record }}}
     <Types :value.sync="record.type"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    <NumberPad @update:value="onUpdateAmount" @update:note="onUpdateNote"/>
+    <NumberPad @submit="saveRecord" @update:value="onUpdateAmount" @update:note="onUpdateNote"/>
   </Layout>
 </template>
 
@@ -12,7 +12,7 @@
   import Types from '@/components/Money/Types.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
   type Record = {
     tags: string[]
@@ -26,6 +26,7 @@
   })
   export default class Money extends Vue {
     tags = ['餐饮', '购物', '居住', '交通', '娱乐', '医疗', '社交'];
+    recordList: Record[]=[];
     record: Record = {
       tags: [], note: '', type: '-', amount: 0
     };
@@ -42,6 +43,17 @@
     }
     onUpdateNote(value: string) {
       this.record.note = value;
+    }
+    saveRecord(){
+      const record2 = JSON.parse (JSON.stringify(this.record))
+      this.recordList.push(record2);
+      // this.recordList.push(this.record);
+    }
+    //这里push的record地址，所以如果进行修改，之前的数据也会跟着改变，因为push进去的地址读取到的是新amount,现在这样会复制一份record
+
+    @Watch('recordList')
+    onRecordListChange(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
     }
   }
 </script>
