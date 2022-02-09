f5a06dcc49254b87e7a356ecc98009b1e04c75a2
Rain <448130604@qq.com>
2022-02-09 15:05:18 +0800

    展示 tag 的 name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名"
+      <FormItem :value="tag.name" field-name="标签名"
                 placeholder="请输入标签名"/>
     </div>
     <div class="deleteTag-wrapper">
@@ -28,6 +28,7 @@
     components: {FormItem, Button}
   })
   export default class EditLabel extends Vue {
+    tag?: { id: string, name: string } = undefined;
     created() {
       const id = this.$route.params.id;
       // route 用来获取路由信息
@@ -36,7 +37,7 @@
       const tag = tags.filter(t => t.id === id)[0];
       // filter 筛选
       if (tag) {
-        console.log(tag);
+        this.tag = tag;
       } else {
         this.$router.replace('/404');
         // router 用来路由器转发
