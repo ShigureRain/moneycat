4c4092dfafb0b9e116fcc51b365855712fd7a15f
Rain <448130604@qq.com>
2022-02-10 21:10:34 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,16 +19,18 @@
   import {Component} from 'vue-property-decorator';
 
   @Component({
-    computed:{
+    computed: {
       tagList() {
-        // return this.$store.fetchTags();
-        return[]
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
 
+    created() {
+      this.$store.commit('fetchTags');
+    }
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
       if (index >= 0) {
@@ -43,7 +45,7 @@
       if (!name) {
         return window.alert('标签名不能为空');
       }
-      // this.$store.createTag(name);
+      this.$store.commit('createTag', name);
     }
   }
 </script>
