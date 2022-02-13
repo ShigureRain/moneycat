beb064f8ce407eea179651771cd21134b015d7c5
Rain <448130604@qq.com>
2022-02-13 22:44:53 +0800

    添加 pc 端二维码，保证手机体验页面

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
+
 Vue.config.productionTip = false;
 
 Vue.component('Nav', Nav);
@@ -17,3 +18,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证效果喵');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
